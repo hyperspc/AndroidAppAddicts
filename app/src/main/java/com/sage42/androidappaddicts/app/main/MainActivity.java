b540670134a8832e35fee0541bc52a9d27ac4a8c
yen <the9sword@gmail.com>
2014-01-10 16:16:44 +0800

    changes on main menu list

--- app/src/main/java/com/sage42/androidappaddicts/app/main/MainActivity.java
+++ app/src/main/java/com/sage42/androidappaddicts/app/main/MainActivity.java
@@ -115,6 +115,7 @@ public class MainActivity extends Activity implements IMenuDrawCallbacks
         }
 
         this.getActionBar().setDisplayHomeAsUpEnabled(true);
+        this.mDrawerToggle.syncState();
     }
 
     @Override
--- app/src/main/java/com/sage42/androidappaddicts/app/menu/MenuData.java
+++ app/src/main/java/com/sage42/androidappaddicts/app/menu/MenuData.java
@@ -17,9 +17,9 @@ public class MenuData
     {
         MENU_ITEMS = new ArrayList<MenuItem>();
         // MenuData.MENU_ITEMS.add(new MenuItem(R.drawable.ic_drawer, R.string.fragment_title, true));
-        MenuData.MENU_ITEMS.add(new MenuItem(R.drawable.ic_launcher, R.string.fragment_episode_title, false));
-        MenuData.MENU_ITEMS.add(new MenuItem(R.drawable.ic_launcher, R.string.fragment_category_title, false));
-        MenuData.MENU_ITEMS.add(new MenuItem(R.drawable.ic_launcher, R.string.fragment_suggest_title, false));
-        MenuData.MENU_ITEMS.add(new MenuItem(R.drawable.ic_launcher, R.string.fragment_host_title, false));
+        MenuData.MENU_ITEMS.add(new MenuItem(R.drawable.ic_storage, R.string.fragment_episode_title, false));
+        MenuData.MENU_ITEMS.add(new MenuItem(R.drawable.ic_grid, R.string.fragment_category_title, false));
+        MenuData.MENU_ITEMS.add(new MenuItem(R.drawable.ic_rating_good, R.string.fragment_suggest_title, false));
+        MenuData.MENU_ITEMS.add(new MenuItem(R.drawable.ic_mic, R.string.fragment_host_title, false));
     }
 }
