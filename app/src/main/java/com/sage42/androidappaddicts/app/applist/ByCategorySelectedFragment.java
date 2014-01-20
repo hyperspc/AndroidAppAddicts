d89e1dce59f61771203811efc88c52681b8a43d4
Corey Scott <corey.scott@gmail.com>
2014-01-20 20:40:29 +0800

    fix license headers

--- app/src/main/java/com/sage42/androidappaddicts/app/applist/ByCategorySelectedFragment.java
+++ app/src/main/java/com/sage42/androidappaddicts/app/applist/ByCategorySelectedFragment.java
@@ -1,10 +1,17 @@
 /**
- * Copyright (C) 2013-2014 Sage 42 Apps Sdn Bhd Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and limitations under the
- * License.
+ *  Copyright (C) 2013-2014 Sage 42 Apps Sdn Bhd
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.sage42.androidappaddicts.app.applist;
 
@@ -47,8 +54,9 @@ public class ByCategorySelectedFragment extends Fragment
         { R.id.category_list_image, R.id.category_list_title, R.id.category_list_desc, R.id.category_type };
 
         final List<HashMap<String, String>> data = this.getData();
-        final SimpleAdapter adapter = new SimpleAdapter(this.getActivity(), data, R.layout.applist_by_category_selected_list_item,
-                        listviewItemCollectionDesc, listviewItemCollection);
+        final SimpleAdapter adapter = new SimpleAdapter(this.getActivity(), data,
+                        R.layout.applist_by_category_selected_list_item, listviewItemCollectionDesc,
+                        listviewItemCollection);
 
         this.mListView.setAdapter(adapter);
 
